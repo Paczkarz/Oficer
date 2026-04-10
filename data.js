24bd18fa0df7701e456254a475ee07949f8bafb3
Paczkarz <miszi333@wp.pl>
2026-04-10 18:38:50 +0200

    Update data.js

--- data.js
+++ data.js
@@ -7722,7 +7722,7 @@ const quizData = {
       "image": "AHS DANA.png",
       "odpowiedzi": ["M120K RAK", "2S1 GOŹDZIK", "AHS DANA", "AHS KRAB"],
       "poprawne": ["AHS DANA"]
-    }.
+    },
             {
       "id": 686,
       "dzial": "SYLWETKI - ROZPOZNANIE",
